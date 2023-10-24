9cc8227a2a81d06a4c5aa7ed73f4c9880421d1a4
Ezequiel Lino <sonisombra@gmail.com>
2023-10-24 10:58:09 -0600

    creacion de los componentes

---
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zoom-range-page',
+  templateUrl: './zoom-range-page.component.html',
+  styleUrls: ['./zoom-range-page.component.css']
+})
+export class ZoomRangePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
