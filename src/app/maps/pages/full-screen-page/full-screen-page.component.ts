efd99045f2691739569f5d83191b99bde321d1d9
Ezequiel Lino <sonisombra@gmail.com>
2023-10-25 11:20:12 -0600

    mostramos el mapa en pantalla completa

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiYnlyb245IiwiYSI6ImNsbzRuaGxneTAzNnMyam1wN3Ixb2tmNjQifQ.agmBURIJruoOt_Ywprb7OA';
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.css']
 })
-export class FullScreenPageComponent implements OnInit {
+export class FullScreenPageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
   }
 
 }
