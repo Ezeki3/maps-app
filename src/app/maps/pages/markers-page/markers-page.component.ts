dedcbb362c81a1c959c73c25f5fe0577d30985e4
Ezequiel Lino <sonisombra@gmail.com>
2023-10-31 10:56:52 -0600

    poner marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-markers-page',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarkersPageComponent implements OnInit {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-104.73395034880717, 39.79945571223877)
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+    
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat,
+      zoom: 10, // starting zoom
+    });
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'PUNTO'
+    // // markerHtml.style.backgroundImage = "url('/assets/images/marker_red.png')";
+
+    // const marker = new Marker({
+    //   color: "red",
+    //   // element: markerHtml
+    // }).setLngLat(this.currentLngLat).addTo(this.map);
+
+  }
 }
