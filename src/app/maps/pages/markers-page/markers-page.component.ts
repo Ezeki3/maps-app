601deab5af31bb783ed33f6f7b234a01b9f0fbea
Ezequiel Lino <sonisombra@gmail.com>
2023-10-31 11:38:07 -0600

    color del texto en funcion del color del fondo de los markers

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,12 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+interface MarkerAndColor{
+  color: string,
+  marker: Marker,
+  colorText: string,
+}
+
 @Component({
   selector: 'app-markers-page',
   templateUrl: './markers-page.component.html',
@@ -9,6 +15,8 @@ import { LngLat, Map, Marker } from 'mapbox-gl';
 export class MarkersPageComponent implements OnInit {
 
   @ViewChild('map') divMap?: ElementRef;
+
+  public markers: MarkerAndColor[] = [];
 
   public zoom: number = 10;
   public map?: Map;
@@ -44,6 +52,7 @@ export class MarkersPageComponent implements OnInit {
   createMarker() {
     if (!this.map ) return;
 
+    //genera un color aleatorio
     const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
     const lngLat = this.map.getCenter();
     
@@ -53,9 +62,29 @@ export class MarkersPageComponent implements OnInit {
   addMarker( lngLat: LngLat, color:string ){
     if( !this.map ) return;
 
+     //funcion para calcular el color del texto
+     const contrastColor = (c: string)=>["#000","#fff"][~~([.299,.587,.114].reduce((r,v,i)=>parseInt(c.substr(i*2+1,2),16)*v+r,0)<128)];
+ 
+     //definir el color del texto basado en el color del marcador
+     const colorText = contrastColor(color);
+
     const marker = new Marker({
       color: color,
       draggable: true
     }).setLngLat( lngLat ).addTo( this.map );
+
+    // this.markers.push({ color, marker });
+    
+    //agregar el color del texto al marcador
+    this.markers.push({
+      color,
+      colorText,
+      marker: marker
+    });
+  }
+
+  deleteMarker( index: number){
+    this.markers[index].marker.remove();
+    this.markers.splice( index, 1);
   }
 }
