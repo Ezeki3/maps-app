572fd230261bfdf4cc9e3b3daec86a40e9ab74e8
Ezequiel Lino <sonisombra@gmail.com>
2023-11-02 10:48:38 -0600

    creacion del componente mini-mapa

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
 export class MiniMapComponent implements OnInit {
 
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap?.nativeElement) throw 'Map Div not found';
+    if (!this.lngLat) throw "Lnglat can´t be null";
+    
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map)
+
   }
 
 }
