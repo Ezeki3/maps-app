4bdd11f1c8d55c766a7d723ed08b485e03af9238
Ezequiel Lino <sonisombra@gmail.com>
2023-11-06 11:13:58 -0600

    standalone - como componente reutilizable

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   selector: 'app-alone-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
