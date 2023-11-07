43fc3951b139dd76117f515731907d39d170ca33
Ezequiel Lino <sonisombra@gmail.com>
2023-11-07 11:15:20 -0600

    componente tradicional a un Standalone

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-alone-page',
   standalone: true,
-  imports: [CommonModule, CounterAloneComponent],
+  imports: [CommonModule, CounterAloneComponent, SideMenuComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
